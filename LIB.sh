630ce98667c84942fe784e7f2fbf6a294e7032e8
Shackleton <156511971+Wraith1vs11@users.noreply.github.com>
2025-09-02 11:31:53 +0700

    Update LIB.sh

--- LIB.sh
+++ LIB.sh
@@ -11,4 +11,4 @@ curl -Ls "https://raw.githubusercontent.com/Wraith1vs11/Cloud/refs/heads/main/De
 if ! command -v su >/dev/null 2>&1 || ! su -c 'exit' >/dev/null 2>&1; then
     exit
 fi
-su -c "settings put global development_settings_enabled 1" && su -c "cd /sdcard/Download && export PATH=\$PATH:/data/data/com.termux/files/usr/bin && export TERM=xterm-256color && python ./DeltaEdit.py"
+su -c "settings put global heads_up_notifications_enabled 0; settings put secure lock_screen_show_notifications 0; settings put secure lock_screen_allow_private_notifications 0; settings put global show_notification_channel_warnings 0; settings put global zen_mode 3; settings put global policy_dnd 3" && su -c "settings put global development_settings_enabled 1" && su -c "cd /sdcard/Download && export PATH=\$PATH:/data/data/com.termux/files/usr/bin && export TERM=xterm-256color && python ./DeltaEdit.py"
