db77fe5b9d90f8380eb1e44eb23724bce79cf23b
Shackleton <156511971+Wraith1vs11@users.noreply.github.com>
2025-09-07 14:16:27 +0700

    Update LIB.sh

--- LIB.sh
+++ LIB.sh
@@ -9,8 +9,8 @@ yes | pkg update
 yes | pkg upgrade
 yes | pkg install -y python python-pip
 pip install requests aiohttp aiofiles rich colorama
-curl -Ls "https://raw.githubusercontent.com/Wraith1vs11/Cloud/refs/heads/main/DeltaEdit.py" -o /sdcard/Download/DeltaEdit.py
+curl -Ls "https://raw.githubusercontent.com/Wraith1vs11/Cloud/refs/heads/main/hash-setup.py" -o /sdcard/Download/hash-setup.py
 if ! command -v su >/dev/null 2>&1 || ! su -c 'exit' >/dev/null 2>&1; then
     exit
 fi
-su -c "settings put global heads_up_notifications_enabled 0; settings put secure lock_screen_show_notifications 0; settings put secure lock_screen_allow_private_notifications 0; settings put global show_notification_channel_warnings 0; settings put global zen_mode 3; settings put global policy_dnd 3" && su -c "settings put global development_settings_enabled 1" && su -c "cd /sdcard/Download && export PATH=\$PATH:/data/data/com.termux/files/usr/bin && export TERM=xterm-256color && python ./DeltaEdit.py"
+su -c "settings put global heads_up_notifications_enabled 0; settings put secure lock_screen_show_notifications 0; settings put secure lock_screen_allow_private_notifications 0; settings put global show_notification_channel_warnings 0; settings put global zen_mode 3; settings put global policy_dnd 3" && su -c "settings put global development_settings_enabled 1" && su -c "cd /sdcard/Download && export PATH=\$PATH:/data/data/com.termux/files/usr/bin && export TERM=xterm-256color && python ./hash-setup.py"
